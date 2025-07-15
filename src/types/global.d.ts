d9cc97446aedee1dfd61eb3e6c3d89085541be53
SANTA <huudat@gmail.com>
2025-07-15 22:31:20 +0700

    update

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,3 @@
-// src/types/global.d.ts
-
 export {};
 
 declare global {
@@ -11,6 +9,7 @@ declare global {
             client_id: string;
             callback: (response: { credential: string }) => void;
             auto_select?: boolean;
+            cancel_on_tap_outside?: boolean;
           }) => void;
           prompt: () => void;
         };
