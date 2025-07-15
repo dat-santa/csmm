00249ad24fc478b170e7671e21f25e8a9194c218
SANTA <huudat@gmail.com>
2025-07-15 21:45:25 +0700

    update

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,27 @@
 //khai báo window.google
 
-declare global {
+interface CredentialResponse {
+    credential: string;
+    select_by: string;
+    clientId?: string;
+  }
+  
+  interface GoogleOneTap {
+    accounts: {
+      id: {
+        initialize: (options: {
+          client_id: string;
+          callback: (response: CredentialResponse) => void;
+          auto_select?: boolean;
+        }) => void;
+        prompt: () => void;
+      };
+    };
+  }
+  
+  declare global {
     interface Window {
-      google: any; // hoặc dùng kiểu chuẩn hơn nếu muốn
+      google: GoogleOneTap;
     }
   }
   
