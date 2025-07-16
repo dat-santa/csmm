796d03aa78ef0bb493edec5f52a3932203e80bab
SANTA <huudat@gmail.com>
2025-07-16 14:28:45 +0700

    up

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   // 👇 Thêm domain dev mà bạn truy cập
+  allowedDevOrigins: ['https://mxh.csmm.vn'],
+  
   images: {
     domains: ["lh3.googleusercontent.com"], // ✅ Cho phép ảnh từ Google OAuth
   },
