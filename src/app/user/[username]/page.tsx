ccaea6ab5fb2fab6dcef5ef0f5347be3d14aae38
SANTA <huudat@gmail.com>
2025-07-16 12:01:58 +0700

    Update page.tsx

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,7 +2,11 @@ type Props = {
     params: { username: string };
   };
   
-  export default async function UserPage({ params }: Props) {
+  export default async function UserPage({ 
+    params,
+     }: {
+    params: { username: string };
+     }) {
     return (
       <div className="flex h-screen items-center justify-center text-2xl">
         Xin chào @{params.username}
